b0cde953c9527035e97aed9e5c1caf175f0eb041
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-10-27 17:30:03 +0200

    crud operations in mongo db

---
+++ SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoCategoryData
+{
+    public MongoCategoryData(IDbConnection db, IMemoryCache cache)
+    {
+        
+    }
+}
