7d03648bf7a21d80504c4338af0aaf79b72e5ecb
Alicja Kowalska <niemogeznalezclaptopa@gmail.com>
2023-10-27 13:19:08 +0200

    status model

--- SuggestionAppLibrary/Models/StatusModel.cs
+++ SuggestionAppLibrary/Models/StatusModel.cs
@@ -1,10 +1,10 @@
 ﻿namespace SuggestionAppLibrary.Models;
 
-public class CategoryModel
+public class StatusModel
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
-    public string CategoryName { get; set; }
-    public string CategoryDescription { get; set; }
+    public string StatusName { get; set; }
+    public string StatusDescription { get; set; }
 }
